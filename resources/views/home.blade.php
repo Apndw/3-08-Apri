a794f8b093040061beb63541c62824837b3a7cd3
Apndw <3103120028@student.smktelkom-pwt.sch.id>
2022-01-18 06:42:37 +0700

    Upload All File

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTS-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <title>Apri | Home</title>
+    </head>
+    <body>
+        <h1>Halaman Home </h1>
+        <h3> Apri Pandu Wicaksono </h3>
+        <p> pandu300478@gmail.com </p>
+        <img src="images/apri.png" alt="apri" width="200px">
+    </body>
+</html>
